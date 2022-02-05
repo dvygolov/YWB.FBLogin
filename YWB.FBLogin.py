124057271d787e260ffe5373e46a5cd7af6cf88b
Daniel Vygolov <rattack@gmail.com>
2022-02-05 18:17:15 +0400

    Refactoring + absolutely changed login and token extraction

--- YWB.FBLogin.py
+++ YWB.FBLogin.py
@@ -5,7 +5,7 @@ import time, requests, re, json
 
 def copyright():
     print()
-    print("               Get Facebook Cookies and Access Token v0.5")
+    print("               Get Facebook Cookies and Access Token v0.7")
     print("   _            __     __  _ _             __          __  _     ")
     print("  | |           \ \   / / | | |            \ \        / / | |    ")
     print("  | |__  _   _   \ \_/ /__| | | _____      _\ \  /\  / /__| |__  ")
@@ -32,6 +32,15 @@ def get_proxies():
         proxies.append({"ip": ps[0], "port": ps[1], "login": ps[2], "password": ps[3]})
     return proxies
 
+def set_proxy(session,proxies,i):
+    proxyindex = i if i < len(pr) - 1 else i % len(pr)
+    cp = proxies[proxyindex]
+    sproxy = {
+        "https",
+        f"https://{cp['login']}:{cp['password']}@{cp['ip']}:{cp['port']}",
+    }
+    session.proxies = sproxy
+    return
 
 def get_accounts():
     accounts = []
@@ -44,14 +53,54 @@ def get_accounts():
 
 
 def login(session, email, password):
+    #set our useragent to some mobile phone
+    hardware_types=[HardwareType.MOBILE.value]
+    user_agent_rotator=UserAgent(hardware_types=hardware_types)
+    user_agent=user_agent_rotator.get_random_user_agent()
+    session.headers.update({
+        'User-Agent': user_agent
+    })
+
     email=email.strip()
     password=password.strip()
+    response = session.get("https://m.facebook.com");
+    #get parameters from login form: lsd, li è m_ts
+    match = re.search('name=\"lsd\"\s+value=\"([^\"]+)\"', response.text)
+    if match == None:
+        print("Can't find lsd value!")
+        return False
+    lsd = match.group(1)
+    match = re.search('name=\"li\"\s+value=\"([^\"]+)\"', response.text)
+    if match == None:
+        print("Can't find li value!")
+        return False
+    li = match.group(1)
+    match = re.search('name=\"m_ts\"\s+value=\"([^\"]+)\"', response.text)
+    if match == None:
+        print("Can't find m_ts value!")
+        return False
+    mts = match.group(1)
+
     response = session.post(
-        "https://m.facebook.com/login.php",
-        data={"email": email, "pass": password},
+        "https://m.facebook.com/login/device-based/regular/login/?shbl=1&refsrc=deprecated",
+        data={
+            "lsd" :lsd,
+            "m_ts": mts,
+            "li":li,
+            "try_number":0,
+            "unrecognized_tries":0,
+            "email": email, 
+            "pass": password,
+            "login": "Log In",
+            "had_cp_prefilled":False,
+            "had_password_prefilled":False,
+            "is_smart_lock":False,
+            "bi_xrwh":0,
+            "_fb_noscript":True
+        },
         allow_redirects=False,
     )
-   if response.status_code == 302:
+    if response.status_code == 302:
         location = response.headers["Location"]
         if "checkpoint" in location:
             print("Checkpoint!")
@@ -64,13 +113,20 @@ def login(session, email, password):
 
 
 def get_token(session):
+    session.headers.update({'User-Agent': 'Mozilla5/0'})
+    session.headers.update({'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8'})
+    session.headers.update({'Accept-Encoding':'gzip'})
+    session.headers.update({'Accept-Language':'ru,en-US;q=0.7,en;q=0.3'})
+    session.headers.update({'Connection':'keep-alive'})
+    session.headers.update({'Sec-Fetch-Dest':'document'})
+    session.headers.update({'Sec-Fetch-Mode':'navigate'})
+    session.headers.update({'Sec-Fetch-Site':'same-origin'})
+    session.headers.update({'Sec-Fetch-User':'?1'})
+    session.cookies.pop('noscript',None)
     response = session.get(
         "https://www.facebook.com/ads/manager?locale=en_US",
-        allow_redirects=False,
+        allow_redirects=True,
     )
-    if response.status_code==302 and "checkpoint" in response.headers['Location']:
-        print("Checkpoint!")
-        return None
     if "checkpoint" in response.url:
         print("Checkpoint!")
         return None
@@ -78,13 +134,11 @@ def get_token(session):
     if match != None:
         adsurl = match.group(1).replace("\\", "")
         response = session.get(adsurl, allow_redirects=True)
-    else:
-        response = session.get(response.url)
-    match = re.search('EAAB[^"]+', response.text)
-    if match:
-        return match.group(0)
-    else:
-        return None
+        match = re.search('EAAB[^"]+', response.text)
+        if match:
+            return match.group(0)
+        else:
+            return None
 
 
 def dump_cookies(sessioncookies):
@@ -103,41 +157,28 @@ def dump_cookies(sessioncookies):
 
 
 if __name__ == "__main__":
-    hardware_types=[HardwareType.MOBILE.value]
-    user_agent_rotator=UserAgent(hardware_types=hardware_types)
     copyright()
     pr = get_proxies()
     accounts = get_accounts()
     i = 0
     with open("parsed.txt", "w") as f:
         for acc in accounts:
-            print(f"Processing account {acc['login']}:{acc['password']}...")
-            proxyindex = i if i < len(pr) - 1 else i % len(pr)
-            cp = pr[proxyindex]
             session = requests.session()
-            user_agent=user_agent_rotator.get_random_user_agent()
-            session.headers.update({
-                'User-Agent': user_agent
-            })
-            sproxy = {
-                "https",
-                f"https://{cp['login']}:{cp['password']}@{cp['ip']}:{cp['port']}",
-            }
-            session.proxies = sproxy
+            print(f"Processing account {acc['login']}:{acc['password']}...")
+            set_proxy(session,pr,i)
             loggedin = login(session, acc["login"], acc["password"])
             if not loggedin:
                 continue
-            session.headers.update({'User-Agent':'Mozilla/5.0'})
             token = get_token(session)
             if token != None:
-                print("Found token and cookies!")
+                print("Found Accesss Token!")
                 acc["cookies"] = json.dumps(dump_cookies(session.cookies))
                 acc["token"] = token
                 f.write(
                     f"{acc['login']}:{acc['password']}:{acc['token']}:{acc['cookies']}\n"
                 )
             else:
-                print("Token and cookies not found(((")
+                print("Token not found!")
             i += 1
 
     print("All done. Accounts with tokens and cookies written to parsed.txt file.")
