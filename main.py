a4bc7dc2de97eb0d4659e1bde96930674ccac558
Daniel Vygolov <rattack@gmail.com>
2023-04-11 21:19:36 +0400

    Changed proxy format

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
+import json
+import requests
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
-import requests, json
+
+import copyright
 from accounts import get_accounts
-from proxies import get_proxies, set_proxy
-from headers import set_useragent, set_headers 
 from biscuits import load_cookies, dump_cookies
-from tndr import get_tinder_access_token
 from fbrequests import login, get_token, get_acc_info
-import copyright 
+from headers import set_useragent, set_headers
+from proxies import get_proxies, set_proxy, update_proxy_link
+from tndr import get_tinder_access_token
 
 if __name__ == "__main__":
     requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
@@ -24,11 +26,16 @@ if __name__ == "__main__":
             set_headers(session)
             set_useragent(session)
             set_proxy(session, pr, i)
-            if acc.cookies!=None:
-                print( "Account has cookies, adding them to request and skipping Log In..." )
+            if acc.cookies is not None:
+                print("Account has cookies, adding them to request and skipping Log In...")
                 load_cookies(session, acc.cookies)
             else:
                 loggedin = login(session, acc.login, acc.password)
+                # if not loggedin:
+                #     update_proxy_link(pr[i])
+                #     set_proxy(session, pr, i)
+                #     loggedin = login(session, acc.login, acc.password)
+
                 acc.cookies = json.dumps(dump_cookies(session.cookies))
                 if not loggedin:
                     continue
@@ -43,7 +50,7 @@ if __name__ == "__main__":
                 ttoken = get_tinder_access_token(session)
                 if ttoken != None:
                     print("Got Tinder app token...")
-                    info = get_acc_info(session,ttoken)
+                    info = get_acc_info(session, ttoken)
                     print("Got account info!")
                     acc.add_info(info)
             f.write(str(acc))
--- proxies.py
+++ proxies.py
@@ -1,59 +1,57 @@
-import requests,json
+import requests, json
+
+
 def get_proxies():
     proxies = []
     pfile = open("proxy.txt", "r")
     plines = pfile.readlines()
     for p in plines:
-        ps = p.strip().split(":")
-        if len(ps) == 4:
-            proxies.append(
-                {
-                    "type": "https",
-                    "ip": ps[0],
-                    "port": ps[1],
-                    "login": ps[2],
-                    "password": ps[3],
-                }
-            )
-        elif len(ps) == 5:
+        linkSplit = p.strip().split("(")
+        if len(linkSplit) == 2:
+            ps = linkSplit[0].split(":")
             proxies.append(
                 {
                     "ip": ps[0],
                     "port": ps[1],
                     "login": ps[2],
                     "password": ps[3],
-                    "link": f"http://{ps[4]}",
-                }
-            )
-        elif len(ps) == 6 and (ps[4] == "http" or ps[4] == "https"):
-            ulink = f"{ps[4]}:{ps[5]}"
-            proxies.append(
-                {
-                    "ip": ps[0],
-                    "port": ps[1],
-                    "login": ps[2],
-                    "password": ps[3],
-                    "link": ulink,
+                    "link": linkSplit[1].strip(')'),
                 }
             )
         else:
-            raise ValueError("Wrong proxy format!")
+            ps = p.strip().split(":")
+            if len(ps) == 4:
+                proxies.append(
+                    {
+                        "type": "https",
+                        "ip": ps[0],
+                        "port": ps[1],
+                        "login": ps[2],
+                        "password": ps[3],
+                    }
+                )
+            else:
+                raise ValueError("Wrong proxy format!")
     return proxies
 
 
 def set_proxy(session, proxies, i):
     proxyindex = i if i < len(proxies) - 1 else i % len(proxies)
     cp = proxies[proxyindex]
-    pstr=f"http://{cp['login']}:{cp['password']}@{cp['ip']}:{cp['port']}"
+    pstr = f"http://{cp['login']}:{cp['password']}@{cp['ip']}:{cp['port']}"
     print(f"Using proxy: {pstr}")
+    sproxy = {"https": pstr, "http": pstr}
+    session.proxies = sproxy
+    return
 
+
+def update_proxy_link(cp):
     if "link" in cp:
         print("Updating proxy ip address using link...")
         response = requests.get(cp["link"], verify=False)
-        if ("Content-Type" in response.headers and response.headers["Content-Type"].startswith("application/json")):
+        if "Content-Type" in response.headers and \
+                response.headers["Content-Type"].startswith("application/json"):
             print("Got response:" + json.dumps(response.text))
         else:
             print("Proxy ip address updated!")
-    sproxy = { "https": pstr, "http": pstr}
-    session.proxies = sproxy
     return
